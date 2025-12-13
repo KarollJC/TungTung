4c92a506eddff2656347af79c7a04874924f0fca
Destr2-2 <modv081214hgtrmca5@cbtis217.edu.mx>
2025-12-12 18:00:36 -0600

    pagina accidentes semicompletada, error con un logn de la barra de navegacion

--- Pages/accidentes motocicleta/crud_accidentesmoto/create.php
+++ Pages/accidentes motocicleta/crud_accidentesmoto/create.php
@@ -1,10 +1,15 @@
 <?php include("db.php"); ?>
+<?php
+session_start();
+$usuario = isset($_SESSION['usuario']) ? $_SESSION['usuario'] : 'Invitado';
+?>
+
 
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Agregar Pregunta</title>
+    <title>Agregar accidentes</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
